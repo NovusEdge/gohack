189671affe0117ba0e2f4b405353d4676f11c425
NovusEdge <novusedge0@gmail.com>
2021-06-10 16:42:51 +0530

    Completed the banner grabber

--- commands/bannerGrabber.go
+++ commands/bannerGrabber.go
@@ -1,35 +1,16 @@
 package main
 
 import (
-	// "flag"
+	"flag"
 	src "gohack/src"
-	// "time"
 )
 
 func main() {
-	// var lower, upper int
-	// var url, protocol string
-	// var timeout time.Duration
+	var url string
+	flag.StringVar(&url, "url", "scanme.nmap.org", "URL for the target to scan.")
 
-	//	flag.IntVar(&lower, "start", 1, "The port to start scanning.")
-	//
-	//	flag.IntVar(&upper, "end", 1024, "The port to end scanning.")
-	//
-	//	flag.StringVar(&url, "url", "scanme.nmap.org", "URL for the target to scan.")
-	//
-	//	flag.DurationVar(&timeout, "timeout", 500, "Timeout for each port scan")
-	//
-	//	flag.StringVar(&protocol, "protocol", "tcp", "Protocol to scan the port on. (tcp/udp)")
-	//
-	//	flag.Parse()
-	//
-	//	ps := src.BannerGrabber{
-	//		URL:      url,
-	//		Protocol: protocol,
-	//	}
+	flag.Parse()
 
-	//	ps.Scan(lower, upper, timeout*time.Millisecond)
-	bg := src.BannerGrabber{URL: "google.com"}
-
+	bg := src.BannerGrabber{URL: url}
 	bg.Grab()
 }
