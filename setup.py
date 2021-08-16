2698453a383519a7b00a733f6c754506e55873bf
NovusEdge <novusedge0@gmail.com>
2021-08-16 16:17:08 +0530

    Updating files & adding base for setup file

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ import shlex
 
 # For timestamps:
 import time
+from datetime import datetime
 
 # For info on the system configuration:
-import platform
+# import platform
+
+# For verbose:
+import argparse
 
 # For system utilities
 import pathlib
@@ -27,25 +31,47 @@ import colorama
 ##################################################
 
 
-PATH = pathlib.Path(__file__).parent().absolute()
+# Changing pwd to parent-directory (of script) for the CLI.
+PATH = pathlib.Path(__file__).parent.absolute()
 os.chdir(PATH)
 
 
-_os = platform.system()
-if _os == "Windows":
-    command = shlex.split("powershell .config/setup_windows.ps")
-    subprocess.run(command)
+# Getting current date and time
+DATE = datetime.now().strftime("%d-%m-%Y")
+TIME = datetime.now().strftime("%H:%M:%S")
+__ERROR_LOGS_PATH = f"logs/setup_errors_{DATE}.log"
 
-elif _os in ["Linux"]:
-    pass
+
+# Creating a log file for the current run of the setup
+if pathlib.Path(__ERROR_LOGS_PATH).exists():
+    __ERROR_LOGS = open(__ERROR_LOGS_PATH, "a")
+    __ERROR_LOGS.write(f"\n\nLog__{TIME}\n\n")
+
 else:
-    pass
+    __ERROR_LOGS = open(__ERROR_LOGS_PATH, "w")
+    __ERROR_LOGS.write(f"\n\nLog__{TIME}\n\n")
+
+
+# Creating a directory for binaries
+if not pathlib.Path("bin/").exists():
+    os.mkdir("bin")
+
+
+# Fetching sys env
+ENV = os.environ
+
+
+# Cleaning old binaries (if present)
+# get_proc = subprocess.call("go get", stderr=__ERROR_LOGS, stdout=sys.stdout, env=ENV)
+os.system("go get")
+
+
+
+# Closing opened files...
+__ERROR_LOGS.close()
 
 
 #go get
 #cd bin/
 #go clean
 #go build ../commands/*
-
-
-
