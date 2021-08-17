7c6d6b87b2cc1bb9e36bc83c5d33aa49ca9147f4
NovusEdge <novusedge0@gmail.com>
2021-08-17 10:27:26 +0530

    Added a tty file for ssh client

---
+++ src/helpers/SSHClientTTY.go
@@ -0,0 +1,24 @@
+package gohack
+
+import (
+    helpers "gohack/src/helpers"
+    "time"
+    "errors"
+    "log"
+)
+
+
+//TTYClient: ...
+/*
+Fields:
+    Client:     [&helpers.Client] ...
+    Timeout:    [time.Duration] ...
+*/
+type TTYClient struct {
+    Client     &helpers.Client
+}
+
+
+func (tc *TTYClient) Start() error {
+    
+}
