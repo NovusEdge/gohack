cb83fd15be3bd6c590f51ffe281cabc83234663c
NovusEdge <novusedge0@gmail.com>
2021-10-13 17:40:51 -0700

    Added Arm() and Disarm()

--- src/lib/logicbomb/LogicBomb.go
+++ src/lib/logicbomb/LogicBomb.go
@@ -1,10 +1,13 @@
 package gohack
 
-import "C"
+// Importing from standard golang libs...
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"time"
+
+	color "gohack/lib"
 )
 
 const (
@@ -24,21 +27,29 @@ type LogicBomb struct {
 	Script   [3]string // Mapping: [3]string{String-content, extension/type, commandline-execution-template}
 }
 
-func (lb *LogicBomb) Implant(location string, deadline time.Time, script [3]string) {
-	lb.BombID = randomHex(32)
+func (lb *LogicBomb) Implant(location string, deadline time.Time, script [3]string) error {
+	lb.BombID, _ = randomHex(32)
 	lb.Location = location
 	lb.Deadline = *deadline
 	lb.Script = script
 
-	C.implant_bomb()
+	fmt.Printf("%s[*] Implanting Logic Bomb...%s\n", color.ColorYellow, color.ColorReset)
+	fmt.Printf("\t%sID: %s \n\tLocation: %s \n\tDeadline: %s\n\tScript: %v%s\n", color.ColorBlue, lb.BombID, ld.Deadline, ld.Location, ld.Script, color.ColorReset)
+
+	// Actually implant the bomb ...
+}
+
+func (lb *LogicBomb) Arm() {
+	// Arm the bomb...
 }
 
 func (lb *LogicBomb) Disarm() {
-
+	// just remove the persistent processes...
+	// dont actually remove the file
 }
 
-func Locate() {
-
+func Locate(bombID string) (path string) {
+	return
 }
 
 func Remove(bombID string) int {
